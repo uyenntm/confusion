d9aa9e19e16cc7b662909170a0bc4a5b2a2885a7
Natalie Nguyen <uyenntm2001@gmail.com>
2019-10-17 22:30:35 -0700

    Function Component

--- src/components/CommentComponent.js
+++ src/components/CommentComponent.js
@@ -1,19 +1,12 @@
-import React, { Component } from "react";
+import React from "react";
 
-
-class Comment extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-  formatDate(val){
-      
+  function formatDate(val){
       let d = new Date(val);
       //console.log(d);
       const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
       return months[d.getMonth()] + " "+ d.getDay()+", "+d.getFullYear();
     }
-  renderComment(dish) {
+  function renderComment(dish) {
     //console.log(dish.comments);
     if (dish != null) {
         //console.log("Comment renderComment");
@@ -23,7 +16,7 @@ class Comment extends Component {
           return (
             <div key={comment.id}>
               <p>{comment.comment}</p>
-              <p>-- {comment.author}, {this.formatDate(comment.date)}</p>
+              <p>-- {comment.author}, {formatDate(comment.date)}</p>
             </div>
           );
         });
@@ -31,21 +24,21 @@ class Comment extends Component {
       } else return <div></div>;
     } else return <div></div>;
   }
-  render() {
+  const Comment =props=> {
     
-    if (this.props.selectedDish != null){
+    if (props.selectedDish != null){
         //console.log("Comment render1");
         return (
           
           <div className="col-12 col-md-5 m-1">
           <h2>Comments</h2>
-            {this.renderComment(this.props.selectedDish)}
+            {renderComment(props.selectedDish)}
           </div>
         );
     }
    
     else return <div></div>;
   }
-}
+
 
 export default Comment;
