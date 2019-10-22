c72ed179d5347977164254f3fce224da9d93b812
Natalie Nguyen <uyenntm2001@gmail.com>
2019-10-21 22:53:02 -0700

    Hw 2

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -10,6 +10,7 @@ import { COMMENTS } from "../shared/comments";
 import { PROMOTIONS } from "../shared/promotions";
 import { LEADERS } from "../shared/leaders";
 import DishDetail from "./DishDetailComponent";
+import About from "./AboutComponent";
 
 class Main extends Component {
   constructor(props) {
@@ -62,6 +63,7 @@ class Main extends Component {
           />
           <Route path="/menu/:dishId" component={DishWithId} />
           <Route exact path="/contactus" component={Contact} />} />
+          <Route exact path="/aboutus" component={()=> <About leaders={this.state.leaders}/>} />} />
           <Redirect to="/home" />
         </Switch>
         <Footer />
